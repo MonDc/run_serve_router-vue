73a8b4285e3a7bf357600c8b6edadc1f3b7887c9
mondc <ahalboob@gmail.com>
2020-02-15 21:41:45 -0500

    the vue part of a project

---
+++ src/components/Todos.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div v-bind:key="todo.id" v-for="todo in todos">
+      <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)" />
+    </div>
+  </div>
+</template>
+
+// _____________________________________________________________________________
+
+<script>
+import TodoItem from "./TodoItem.vue";
+export default {
+  name: "Todos",
+  components: {
+    TodoItem
+  },
+  props: ["todos"]
+};
+</script>
+
+// _____________________________________________________________________________
+
+
+<style scoped>
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="about">
+    <h1>About This App</h1>
+    <p>This app was done using vue.js similar to react but it is a lot easier than react</p>
+    <p>
+      Done by:
+      <strong>Montasir</strong>
+    </p>
+  </div>
+</template>
